c770eb6b0bf6312a71631e5f30e0670fe0b6fd45
Om Lokhande <91490800+rubyproducti9n@users.noreply.github.com>
2025-01-09 18:52:58 +0530

    Minor update

--- App/app/src/main/java/com/rubyproducti9n/unofficialmech/AboutActivity.java
+++ App/app/src/main/java/com/rubyproducti9n/unofficialmech/AboutActivity.java
@@ -32,7 +32,6 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        //Some Text edited! One more
         setupVersionInfo();
         setupDynamicLayout();
         setupToolbar();
